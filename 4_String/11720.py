e5cecc4800de05118ef5bdf821c48271bb42133c
zioni_715 <jennysiw715557@gmail.com>
2025-11-18 17:28:19 +0900

    11.18 Fist Commit

---
+++ 4_String/11720.py
@@ -0,0 +1,24 @@
+# 숫자의 합
+# N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
+
+# 입력 : 첫째 줄에 숫자의 개수 N(1 ≤ N ≤ 100)이 주어진다. 둘째 줄에 숫자 N개가 공백 없이 주어진다.
+# 출력 : 입력으로 주어진 숫자 N개의 합을 출력한다.
+
+# N = int(input())
+
+# numbers = []
+
+# for i in range(N):
+#   numbers.append(int(input())) # append()는 리스트의 맨 뒤에 새로운 요소를 추가하는 함수  
+
+# sum_numbers = sum(numbers)
+# print(sum_numbers)
+
+# M = int(input())
+
+# numbers = list(map(int, input())) # map()은 리스트의 모든 원소에 대해 특정한 함수를 적용할 때 사용하는 함수 
+
+# sum_numbers = sum(numbers)
+# print(sum_numbers)
+
+
