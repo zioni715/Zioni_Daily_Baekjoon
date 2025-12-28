a397f8fde540b1a5753cd2c18508d69e644987a6
zioni_715 <jennysiw715557@gmail.com>
2025-12-28 17:52:10 +0900

    2738

---
+++ 7. 2차원 배열/2738.py
@@ -0,0 +1,46 @@
+# 문제 : 행렬 덧셈
+
+'''
+문제
+N*M 크기의 두 행렬 A와 B가 주어졌을 때, 두 행렬을 더하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 행렬의 크기 N과 M이 주어진다. 
+둘째 줄부터 N개의 줄에 행렬 A의 원소 M개가 차례대로 주어진다.
+이어서 N개의 줄에 행렬 B의 원소 M개가 차례대로 주어진다. 
+N과 M은 100보다 작거나 같고, 행렬의 원소는 절댓값이 100보다 작거나 같은 정수이다.
+
+출력
+첫째 줄부터 N개의 줄에 행렬 A와 B를 더한 행렬을 출력한다. 
+행렬의 각 원소는 공백으로 구분한다.
+'''
+
+import numpy as np
+
+N, M = map(int, input().split())
+A = []
+B = []
+
+A = [list(map(int, input().split())) for _ in range(N)]
+B = [list(map(int, input().split())) for _ in range(N)]
+
+A = np.array(A)
+B = np.array(B)
+
+C = A + B
+print(C)
+# 아 백준 numpy 지원 안함 ㅠㅠ
+
+N, M = map(int, input().split())
+A = []
+B = []
+
+A = [list(map(int, input().split())) for _ in range(N)]
+B = [list(map(int, input().split())) for _ in range(N)]
+
+for i in range(N):
+  for j in range(M):
+    A[i][j] += B[i][j]
+
+for line in A:
+  print(*line)
