da300b765c35a383310994f07590c297f9845ca2
zioni_715 <jennysiw715557@gmail.com>
2026-01-02 16:16:46 +0900

    5086

---
+++ 9. 약수, 배수와 소수 1/5086.py
@@ -0,0 +1,42 @@
+# 문제 : 배수와 약수
+
+'''
+문제
+4 x 3 = 12이다.
+이 식을 통해 다음과 같은 사실을 알 수 있다.
+3은 12의 약수이고, 12는 3의 배수이다.
+4도 12의 약수이고, 12는 4의 배수이다.
+두 수가 주어졌을 때, 다음 3가지 중 어떤 관계인지 구하는 프로그램을 작성하시오.
+1. 첫 번째 숫자가 두 번째 숫자의 약수이다.
+2. 첫 번째 숫자가 두 번째 숫자의 배수이다.
+3. 첫 번째 숫자가 두 번째 숫자의 약수와 배수 모두 아니다.
+
+입력
+입력은 여러 테스트 케이스로 이루어져 있다.
+각 테스트 케이스는 10,000아 넘지않는 두 자연수로 이루어져 있다. 
+마지막 줄에는 0이 2개 주어진다.
+두 수가 같은 경우는 없다.
+
+출력
+각 테스트 케이스마다 첫 번째 숫자가 두 번째 숫자의 약수라면 factor를, 배수라면 multiple을, 둘 다 아니라면 neither를 출력한다.
+'''
+
+def find_relationship(A, B):
+  if B % A == 0:
+    return "factor"
+  elif A % B == 0:
+    return "multiple"
+  else:
+    return "neither"
+  
+result = []
+
+while True:
+  A, B = map(int, input().split())
+
+  if A == 0 and B == 0:
+    break
+
+  result.append(find_relationship(A, B))
+
+print("\n".join(result))
